3a893055c25aa0d912740dad921f13ef38f7b412
22CS020samarthdhol <120238118+22CS020samarthdhol@users.noreply.github.com>
2023-06-29 22:33:40 +0530

    Add files via upload

---
+++ ambiguity_resolve.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+public:
+    void display();
+};
+void base :: display()
+{
+    cout<<"THIS IS BASE CLASS"<<endl;
+}
+//-----------------------------------------------
+class derived : public base
+{
+public:
+    void display();
+};
+void derived :: display()
+{
+    cout<<"THIS IS DERIVED CLASS"<<endl;
+}
+//-----------------------------------------------
+int main()
+{
+    derived d;
+    base b;
+    cout<<endl<<"d.display() called"<<endl;
+    cout<<"-------------------------------------------------"<<endl;
+    d.display();
+    cout<<"-------------------------------------------------"<<endl;
+    cout<<endl<<"b.display() called"<<endl;
+    cout<<"-------------------------------------------------"<<endl;
+    b.display();
+    cout<<"-------------------------------------------------"<<endl;
+    cout<<endl<<"d.base :: display() called"<<endl;
+    cout<<"-------------------------------------------------"<<endl;
+    d.base :: display();
+    cout<<"-------------------------------------------------"<<endl;
+    cout<<endl<<"d.derived :: display() called"<<endl;
+    cout<<"-------------------------------------------------"<<endl;
+    d.derived :: display();
+    cout<<"-------------------------------------------------"<<endl;
+    return 0;
+}
